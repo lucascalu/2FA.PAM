c479d54d3ea18d72212e50c28e109ebb137671e1
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2019-12-04 10:32:28 -0300

    Update pam_acesso2.c

--- pam_acesso2.c
+++ pam_acesso2.c
@@ -32,42 +32,7 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 	return PAM_SUCCESS;
 }
 
-PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
-{
 
-    struct tm *data_hora_atual;
-    time_t segundos;    
-    time(&segundos);   
-    data_hora_atual = localtime(&segundos);  
-
-    ssize_t len;
-     
-    char buf[256], *p;
-    char buf2[256];
-    int i;
-   
-      len = readlink("/sys/block/sdb", buf, 256);
-   
-    buf[len] = 0;
-   
-    // printf("%s\n", buf);
-    sprintf(buf2, "%s/%s", "/sys/block/", buf);
-    for (i=0; i<6; i++) {
-        p = strrchr(buf2, '/');
-        *p = 0;
-    }
-   
-    // printf("%s\n", buf2);
-    strcat(buf2, "/serial");
-    // printf("opening %s\n", buf2);
-
-    int f = open(buf2, 0);
-    len = read(f, buf, 256);
-    if (len <= 0) {
-        
-    }
- 
-    buf[len - 1 ] = 0;
 
 
       FILE *g= fopen("/etc/pam.d/pam.acesso/acesso.serial", "r");
@@ -94,29 +59,17 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
       fclose(g);
 
       if(estado == 0){
+	     
+	    
         system("clear");
         
-                FILE *log;
-
-                log = fopen("/etc/pam.d/pam.acesso/log/login", "a");
-                fprintf(log, "%s", "-> Serial: ");
-                fprintf(log, "%s | ", buf);
-                fprintf(log, "%d/", data_hora_atual->tm_mday);
-                fprintf(log, "%d/", data_hora_atual->tm_mon+1);
-                fprintf(log, "%d ", data_hora_atual->tm_year+1900);
-                fprintf(log, "%d:", data_hora_atual->tm_hour);
-                fprintf(log, "%d:", data_hora_atual->tm_min);
-                fprintf(log, "%d\n ", data_hora_atual->tm_sec);
-
+             
           printf("\n\n  -> Acesso Liberado!");
           printf("\n          -> Logando..\n\n");
           sleep(2);
           return PAM_SUCCESS;
       }else{
-        system("clear");
-          printf("\n\n  -> Acesso bloqueado!");
-          printf("\n          ->  Tente novamente.\n\n");
-          sleep(2);
+        
           return PAM_AUTH_ERR;
       }
 }
